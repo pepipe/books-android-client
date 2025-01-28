674c386c0e263b8afd14ff0e6e2e6005e8a7502b
pepipe <pepipee@gmail.com>
2025-01-28 22:56:51 +0000

    Initial android java project

---
+++ app/src/main/java/com/example/booksclient/NativeApi.java
@@ -0,0 +1,15 @@
+package com.example.booksclient;
+
+import java.util.List;
+
+public class NativeApi {
+    static {
+        System.loadLibrary("BooksSDK");
+    }
+
+    public static native String fetchBooks(String query, int startIndex, int maxResults);
+    public static native void addToFavorites(String bookId, String bookJson);
+    public static native void removeFromFavorites(String bookId);
+    public static native boolean isFavorite(String bookId);
+    public static native List<String> getFavoriteBooks();
+}
